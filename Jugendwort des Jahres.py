4f9e77450312bf867fd30700613f2050b349d2e8
A. S <admin@sotai.tk>
2020-07-05 13:39:30 +0200

    Code vereinfacht und umstrukturiert.

--- Jugendwort des Jahres.py
+++ Jugendwort des Jahres.py
@@ -1,88 +1,63 @@
 import requests
 import time
 import re
-bold_start = '\033[1m'
-bold_end   = '\033[0m'
+import sys
 
-alter10bis15 = '3067519627'
-alter16bis20 = '3067519628'
-alter21bis30 = '3067519629'
-alter30undmehr = '3067519630'
-AbstimmungsUrl = 'https://www.surveymonkey.com/r/7JZRVLJ?embedded=1'
-Wort = 'Hurensohn'
 
-i = 0
+# General values
+i           = 0
+done_string = '\033[1mFertig\033[0m'
+survey_url  = 'https://www.surveymonkey.com/r/7JZRVLJ?embedded=1'
+age_groups  = [
+("3067519627", "10 bis 15 Jahre"),
+("3067519628", "16 bis 20 Jahre"),
+("3067519629", "21 bis 30 Jahre"),
+("3067519630", "über 30 Jahre")
+]
 
-while i < 10:
-    print('warte 5 Sekunden')
-    time.sleep(5)
-    print('beziehe Daten vom Server')
-    r = requests.get(AbstimmungsUrl)
-    survey_data = re.search(r'survey_data" value="(.*?)" />', r.text).group(1)
-    daten1 = {
-        '463803414': alter10bis15,
-        '463803684': Wort,
-        '483089934[]': '3189794655',
-        'is_previous': 'false',
-        'disable_survey_buttons_on_submit': '',
-        'survey_data': survey_data
-    }
-    print('warte 5 Sekunden')
-    time.sleep(5)
-    print('stimme ab mit Alter 10 bis 15')
-    r1 = requests.post(AbstimmungsUrl, data = daten1)
-    print(bold_start, 'Abgestimmt mit Alter 10 bis 15!', bold_end)
-    print('warte 5 Sekunden')
-    time.sleep(5)
-    print('beziehe Daten vom Server')
-    r = requests.get(AbstimmungsUrl)
-    survey_data = re.search(r'survey_data" value="(.*?)" />', r.text).group(1)
-    daten2 = {
-        '463803414': alter16bis20,
-        '463803684': Wort,
-        '483089934[]': '3189794655',
-        'is_previous': 'false',
-        'disable_survey_buttons_on_submit': '',
-        'survey_data': survey_data
-    }
-    print('warte 5 Sekunden')
-    time.sleep(5)
-    print('stimme ab mit Alter 16 bis 20')
-    r1 = requests.post(AbstimmungsUrl, data = daten2)
-    print(bold_start, 'Abgestimmt mit Alter 16 bis 20!', bold_end)
-    print('warte 5 Sekunden')
-    time.sleep(5)
-    print('beziehe Daten vom Server')
-    r = requests.get(AbstimmungsUrl)
-    survey_data = re.search(r'survey_data" value="(.*?)" />', r.text).group(1)
-    daten3 = {
-        '463803414': alter21bis30,
-        '463803684': Wort,
-        '483089934[]': '3189794655',
-        'is_previous': 'false',
-        'disable_survey_buttons_on_submit': '',
-        'survey_data': survey_data
-    }
-    print('warte 5 Sekunden')
-    time.sleep(5)
-    print('stimme ab mit Alter 21 bis 30')
-    r1 = requests.post(AbstimmungsUrl, data = daten3)
-    print(bold_start, 'Abgestimmt mit Alter 21 bis 30!', bold_end)
-    print('warte 5 Sekunden')
-    time.sleep(5)
-    print('beziehe Daten vom Server')
-    r = requests.get(AbstimmungsUrl)
-    survey_data = re.search(r'survey_data" value="(.*?)" />', r.text).group(1)
-    daten4 = {
-        '463803414': alter30undmehr,
-        '463803684': Wort,
-        '483089934[]': '3189794655',
-        'is_previous': 'false',
-        'disable_survey_buttons_on_submit': '',
-        'survey_data': survey_data
-    }
-    print('warte 5 Sekunden')
-    time.sleep(5)
-    print('stimme ab mit Alter 30+')
-    r1 = requests.post(AbstimmungsUrl, data = daten4)
-    print(bold_start, 'Abgestimmt mit Alter 30+!', bold_end)
+
+# User-defined values, tweak as you wish
+word          = 'Hurensohn' # The word to submit
+iterations    = 10          # The number of times to submit the word for each age group
+time_interval = 5           # The time interval between submissions in seconds
+
+
+# Setup
+print(f"Beziehe Daten vom Server...", end="")
+sys.stdout.flush()
+r = requests.get(survey_url)
+
+survey_data = re.search(r'survey_data" value="(.*?)" />', r.text).group(1)
+post_data = {
+	'463803414'  : "",
+	'463803684'  : word,
+	'483089934[]': '3189794655',
+	'is_previous': 'false',
+	'survey_data': survey_data,
+	'disable_survey_buttons_on_submit': ''
+}
+
+print(done_string)
+
+
+# Main loop
+while i < iterations:
+	
+	# Go through each age group
+	for age_key, age_string in age_groups:
+		
+		# Set age group for next submission
+		post_data["463803414"] = age_key
+		
+		# Wait the specified amount of time
+		print(f"Warte {time_interval} Sekunde(n)...", end="")
+		sys.stdout.flush()
+		time.sleep(time_interval)
+		print(done_string)
+		
+		# Submit survey
+		print(f'Stimme ab mit der Altersgruppe "{age_string}"...', end="")
+		sys.stdout.flush()
+		requests.post(survey_url, data=post_data)
+		print(done_string)
+		print()
